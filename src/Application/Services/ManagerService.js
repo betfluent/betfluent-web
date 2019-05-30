c63b0cdde1a908be730b305ec5ba081923922b97
James Livolsi <james.livolsi08@gmail.com>
2019-05-29 23:16:20 -0400

    staging-fe

--- src/Application/Services/ManagerService.js
+++ src/Application/Services/ManagerService.js
@@ -5,7 +5,15 @@ import Bet from "../Models/Bet";
 import Game from "../Models/Game";
 import Fund from "../Models/Fund";
 
-const BASE_URL = "https://boston-02108.herokuapp.com/api/";
+let BASE_URL;
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.REACT_APP_FRONTEND_ENV === "debug"
+) {
+  BASE_URL = "https://providence-02108.herokuapp.com/api/";
+} else {
+  BASE_URL = "https://boston-02108.herokuapp.com/api/";
+}
 
 function getListFeed(ref, callback, Model) {
   const feed = ref.on("value", snapshot => {
--- src/firebase.js
+++ src/firebase.js
@@ -3,14 +3,30 @@ import "firebase/auth";
 import "firebase/database";
 import "firebase/storage";
 
-const config = {
-  apiKey: "AIzaSyA3vie3Ie_GuZiBEf9DilFSLaaxtGLtACs",
-  authDomain: "betfluent-prod.firebaseapp.com",
-  databaseURL: "https://betfluent-prod.firebaseio.com",
-  projectId: "betfluent-prod",
-  storageBucket: "betfluent-prod.appspot.com",
-  messagingSenderId: "1052075330350"
-};
+let config;
+
+if (
+  process.env.NODE_ENV === "development" ||
+  process.env.REACT_APP_FRONTEND_ENV === "debug"
+) {
+  config = {
+    apiKey: "AIzaSyALTKmSmehVCCsXWr_aY9MDmHAfVJhiZOs",
+    authDomain: "betfluent-staging.firebaseapp.com",
+    databaseURL: "https://betfluent-staging.firebaseio.com",
+    projectId: "betfluent-staging",
+    storageBucket: "betfluent-staging.appspot.com",
+    messagingSenderId: "644050862530"
+  };
+} else {
+  config = {
+    apiKey: "AIzaSyA3vie3Ie_GuZiBEf9DilFSLaaxtGLtACs",
+    authDomain: "betfluent-prod.firebaseapp.com",
+    databaseURL: "https://betfluent-prod.firebaseio.com",
+    projectId: "betfluent-prod",
+    storageBucket: "betfluent-prod.appspot.com",
+    messagingSenderId: "1052075330350"
+  }
+}
 
 firebase.initializeApp(config);
 
