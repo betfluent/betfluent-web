7120458df00408f185b593f9d7aae842e4b8ebb6
James Livolsi <james.livolsi08@gmail.com>
2019-05-30 20:07:21 -0400

    Make redirect to www

---
+++ server.js
@@ -0,0 +1,28 @@
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var app = express();
+require('dotenv').config();
+
+app.all(/.*/, function(req, res, next) {
+    if (process.env.NODE_ENV === 'production' && process.env.REACT_APP_FRONTEND_ENV !== 'debug') {
+        var host = req.header("host");
+        if (host.match(/^www\..*/i)) {
+            next();
+        } else {
+            res.redirect(301, "https://www." + host);
+        }
+    }
+    next();
+});
+
+app.use(express.static("build"));
+
+app.use(function(req, res, next) {
+    if (!fs.existsSync(req.originalUrl)) res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    else next();
+});
+
+app.listen(8080, function() {
+    console.log('App ready on port 8080');
+});
